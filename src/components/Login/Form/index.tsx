e6f6d48b8f0c68cf88babb5d1b05f2eb45278ccf
heysafronov <heysafronov@gmail.com>
2019-11-23 19:37:10 +0300

    Internal task #8

--- src/components/Login/Form/index.tsx
+++ src/components/Login/Form/index.tsx
@@ -15,6 +15,58 @@ const FormSection = styled.section`
   border-radius: 20px;
   margin: 35px 10px 25px 10px;
 `
+const InputWrapper = styled.div`
+  display: flex;
+  position: relative;
+`
+const InputLabel = styled.label`
+  position: absolute;
+  top: 0;
+  padding: 0 0 3px 20px;
+  transition: all 200ms;
+  opacity: 0.5;
+`
+const InputText = styled.input`
+  z-index: 1;
+  width: 320px;
+  height: 38px;
+  border: 1px solid #f1f1f5;
+  box-sizing: border-box;
+  border-radius: 15px;
+  background: #fafafb;
+  padding: 0 20px;
+  margin-bottom: 15px;
+  font-size: 12px;
+  :focus {
+    outline: none;
+    ::placeholder {
+      opacity: 0;
+    }
+  }
+  :focus + ${InputLabel} {
+    font-size: 75%;
+    transform: translate3d(0, -100%, 0);
+    opacity: 1;
+  }
+`
+const InputSubmit = styled.input`
+  width: 320px;
+  height: 38px;
+  background: #0062ff;
+  border-radius: 10px;
+  border: none;
+  color: white;
+  font-size: 12px;
+  text-align: center;
+  cursor: pointer;
+  :focus {
+    outline: none;
+  }
+  :disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+`
 
 interface IFormProps {
   checkAuth: typeof checkAuth
@@ -79,23 +131,31 @@ const Form: React.FC<IFormProps> = props => {
     <FormSection>
       <h3>Login to your account</h3>
       <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          placeholder='Your login'
-          name='login'
-          onChange={handleChange}
-          value={user.login}
-        />
+        <InputWrapper>
+          <InputText
+            type='text'
+            placeholder='Your login'
+            name='login'
+            onChange={handleChange}
+            value={user.login}
+          />
+          <InputLabel>Your login</InputLabel>
+        </InputWrapper>
         <span>{error.login}</span>
-        <input
-          type='password'
-          placeholder='Your password'
-          name='password'
-          onChange={handleChange}
-          value={user.password}
-        />
+
+        <InputWrapper>
+          <InputText
+            type='password'
+            placeholder='Your password'
+            name='password'
+            onChange={handleChange}
+            value={user.password}
+          />
+          <InputLabel>Your password</InputLabel>
+        </InputWrapper>
         <span>{error.password}</span>
-        <input type='submit' value='Sign in' />
+
+        <InputSubmit type='submit' value='Sign in' disabled={true} />
       </form>
     </FormSection>
   )
