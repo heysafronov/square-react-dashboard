53e7e7825cdceef589ffb3d16d03f323c2071bb1
heysafronov <heysafronov@gmail.com>
2019-11-26 19:56:31 +0300

    Internal task #11

--- src/components/Common/Sidebar/Header/index.tsx
+++ src/components/Common/Sidebar/Header/index.tsx
@@ -4,15 +4,15 @@ import HeaderItems from 'components/Common/Sidebar/Header/HeaderItems'
 
 const HeaderWrapper = styled.div`
   height: 230px;
-  padding: 40px 20px 20px 20px;
+  padding: 20px 20px 10px 20px;
   display: flex;
   justify-content: center;
   align-items: center;
 `
 const HeaderMain = styled.div`
-  height: 210px;
+  height: 190px;
   width: 230px;
-  border-bottom: 1px solid #F1F1F5;
+  border-bottom: 1px solid #f1f1f5;
 `
 
 const Header = () => {
