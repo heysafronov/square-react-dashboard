17eb019817941087502830742a20e4862869cfe3
heysafronov <heysafronov@gmail.com>
2019-11-27 20:32:04 +0300

    Internal task #12

---
+++ src/components/Main/Content/ContentTitle.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+const ContentTitleWrapper = styled.section`
+
+`
+
+const ContentTitle = () => {
+  return (
+    <ContentTitleWrapper>
+      123
+    </ContentTitleWrapper>
+  )
+}
+
+export default ContentTitle
--- src/components/Main/Content/index.tsx
+++ src/components/Main/Content/index.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react'
 import styled from 'styled-components'
+import ContentTitle from 'components/Main/Content/ContentTitle'
 
 const ContentWrapper = styled.section`
   display: flex;
   flex-direction: column;
+  width: 100%;
+  background-color: #fafafa;
+  padding: 40px;
 `
 
 const Content = () => {
   return (
     <ContentWrapper>
-      <span>Text</span>
+      <ContentTitle />
       <span>Teams</span>
       <span>Tasks</span>
     </ContentWrapper>
