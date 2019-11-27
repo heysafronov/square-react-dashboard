989e2089d945742ab8ece806533515fc66286d4e
heysafronov <heysafronov@gmail.com>
2019-11-27 20:38:47 +0300

    Internal task #12

--- src/components/Main/Content/ContentTitle.tsx
+++ src/components/Main/Content/ContentTitle.tsx
@@ -1,15 +1,25 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
-const ContentTitleWrapper = styled.section`
-
+const Username = styled.span`
+  font-size: 24px;
+  text-align: center;
+  letter-spacing: 0.1px;
+  color: #171725;
+`
+const WelcomeText = styled.span`
+  font-size: 18px;
+  letter-spacing: 0.1px;
+  color: #92929d;
+  margin-left: 10px;
 `
 
 const ContentTitle = () => {
   return (
-    <ContentTitleWrapper>
-      123
-    </ContentTitleWrapper>
+    <div>
+      <Username>Hi James,</Username>
+      <WelcomeText>here’s your currently projects</WelcomeText>
+    </div>
   )
 }
 
