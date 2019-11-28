47c40f262528b08bd4ed4dd6c787c3bedb60d2e3
heysafronov <heysafronov@gmail.com>
2019-11-28 14:16:07 +0300

    Internal task #13

---
+++ src/components/Common/TeamCard/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+interface ITeamCard {
+
+}
+
+const TeamCard = () => {
+  return (<div>1</div>)
+}
+
+export default TeamCard
