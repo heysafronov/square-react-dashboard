d0e7bfc32c1219613ba7b047fcfba3283cbb956f
heysafronov <heysafronov@gmail.com>
2019-11-30 18:53:09 +0300

    Internal task #15

---
+++ src/components/Tasks/Content/ContentTitle.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+const Username = styled.span`
+  font-size: 24px;
+  text-align: center;
+  letter-spacing: 0.1px;
+  color: #171725;
+  @media (max-width: 450px) {
+    display: none;
+  }
+`
+const WelcomeText = styled.span`
+  font-size: 18px;
+  letter-spacing: 0.1px;
+  color: #92929d;
+  margin-left: 10px;
+  font-family: 'Roboto', sans-serif;
+`
+
+const ContentTitle = () => {
+  return (
+    <div>
+      <Username>Hi James,</Username>
+      <WelcomeText>here’s your currently projects</WelcomeText>
+    </div>
+  )
+}
+
+export default ContentTitle
--- src/components/Tasks/Content/index.tsx
+++ src/components/Tasks/Content/index.tsx
@@ -1,25 +1,37 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
-import TaskModule from 'components/Common/TaskWrapper'
+import TaskWrapper from 'components/Common/TaskWrapper'
+import ContentTitle from 'components/Tasks/Content/ContentTitle'
 import {
   filtratedTasksBacklog,
   filtratedTasksTodo
 } from 'store/tasks/selectors'
 
 const Wrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 30px;
-  width: 600px;
-  height: 600px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  min-width: 250px;
+  background-color: #fafafa;
+  padding: 40px;
+  @media (max-width: 450px) {
+    padding: 10px;
+  }
+`
+const Tasks = styled.div`
+  margin: 35px 0 20px 0;
+  display: flex;
 `
 
 const Content = props => {
   return (
     <Wrapper>
-      <TaskModule data={props.backlog} type='backlog' />
-      <TaskModule data={props.todo} type='todo' />
+      <ContentTitle />
+      <Tasks>
+        <TaskWrapper data={props.backlog} type='backlog' />
+        <TaskWrapper data={props.todo} type='todo' />
+      </Tasks>
     </Wrapper>
   )
 }
