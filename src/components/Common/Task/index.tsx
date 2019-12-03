75241a8a7bf549d4e4ef6720ed85c505098c8a84
heysafronov <heysafronov@gmail.com>
2019-12-03 18:07:08 +0300

    Internal task #17

--- src/components/Common/Task/index.tsx
+++ src/components/Common/Task/index.tsx
@@ -1,17 +1,40 @@
 import * as React from 'react'
+import { AppState } from 'store'
+import { connect } from 'react-redux'
 import styled from 'styled-components'
 import { ITaskState } from 'store/tasks/types'
+import { getKanbanOption } from 'store/show/selectors'
 
 const Wrapper = styled.div`
-  width: 50px;
-  height: 50px;
-  background-color: red;
-  color: white;
+  display: flex;
+  flex-direction: ${(props: ITaskProps) =>
+    props.kanbanOption ? 'column' : 'row'};
+  padding-bottom: 20px;
+`
+const Titles = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: cadetblue;
+`
+const Info = styled.div`
+  display: flex;
+  background-color: bisque;
+`
+const Score = styled.div`
+  display: flex;
+  flex-direction: ${(props: ITaskProps) =>
+    props.kanbanOption ? 'column' : 'row'};
+  background-color: cornflowerblue;
+`
+const Avatars = styled.div`
+  display: flex;
+  background-color: goldenrod;
 `
 
 interface ITaskProps {
   data: ITaskState
   key: string
+  kanbanOption: boolean
 }
 
 const Task: React.FC<ITaskProps> = props => {
@@ -20,10 +43,30 @@ const Task: React.FC<ITaskProps> = props => {
   }
 
   return (
-    <Wrapper draggable={true} onDragStart={onDragStart}>
-      <span>{props.data.name}</span>
+    <Wrapper {...props} draggable={true} onDragStart={onDragStart}>
+      <Titles>
+        <span>{props.data.name}</span>
+        <span>Team</span>
+      </Titles>
+      <Info>
+        <div>13</div>
+        <div>4/5</div>
+        <div>2 days left</div>
+      </Info>
+      <Score>
+        <div>100%</div>
+        <div>---------</div>
+      </Score>
+      <Avatars>
+        <div>ava 1</div>
+        <div>ava 2</div>
+        <div>ava 3</div>
+      </Avatars>
     </Wrapper>
   )
 }
+const mapStateToProps = (state: AppState) => ({
+  kanbanOption: getKanbanOption(state)
+})
 
-export default Task
+export default connect(mapStateToProps)(Task)
