cb9f7cc14f8e903d0ffccc72b2ad89f5b0642377
heysafronov <heysafronov@gmail.com>
2019-12-08 17:25:24 +0300

    Internal task #23

--- src/api/index.js
+++ src/api/index.js
@@ -1,74 +1,74 @@
 export const normalizedTasks = [
-  {
-    id: '1',
-    title: 'Finance App Exploration',
-    team: 'Iconspace Team',
-    attach: 13,
-    status: '4/5',
-    score: { days: 14, colors: { bg: '#F1F1F5', text: '#92929D' } },
-    line: 85,
-    type: 'Backlog',
-    users: [
-      {
-        size: 32,
-        name: 'KA',
-        color: '#FFC542',
-        avatar: ''
-      },
-      {
-        size: 32,
-        name: 'TE',
-        color: '',
-        avatar: 'https://i.imgur.com/xyev5eL.png'
-      },
-      {
-        size: 32,
-        name: 'AS',
-        color: '#A461D8',
-        avatar: ''
-      }
-    ]
-  },
-  {
-    id: '6',
-    title: 'Product Preview & Mock',
-    team: 'Iconspace Team',
-    attach: 13,
-    status: '4/5',
-    score: { days: 2, colors: { bg: '#FFEEEF', text: '#FC5A5A' } },
-    line: 85,
-    type: 'In Progress',
-    users: [
-      {
-        size: 32,
-        name: 'GU',
-        color: '#FF9AD5',
-        avatar: ''
-      }
-    ]
-  },
-  {
-    id: '10',
-    title: 'Reactjs.org Website',
-    team: 'Uranus Team',
-    attach: 13,
-    status: '10/10',
-    score: { days: 0, color: { bg: '', text: '' } },
-    line: 100,
-    type: 'Complete',
-    users: [
-      {
-        size: 32,
-        name: 'ZT',
-        color: '#82C43C',
-        avatar: ''
-      },
-      {
-        size: 32,
-        name: 'MI',
-        color: '#50B5FF',
-        avatar: ''
-      }
-    ]
-  }
-]
+         {
+           id: '1',
+           title: 'Finance App Exploration',
+           team: 'Iconspace Team',
+           attach: 13,
+           status: '4/5',
+           score: { days: 14, colors: { bg: '#F1F1F5', text: '#92929D' } },
+           line: 85,
+           type: 'Backlog',
+           users: [
+             {
+               size: 32,
+               name: 'KA',
+               color: '#FFC542',
+               avatar: ''
+             },
+             {
+               size: 32,
+               name: 'TE',
+               color: '',
+               avatar: 'https://i.imgur.com/xyev5eL.png'
+             },
+             {
+               size: 32,
+               name: 'AS',
+               color: '#A461D8',
+               avatar: ''
+             }
+           ]
+         },
+         {
+           id: '6',
+           title: 'Product Preview & Mock',
+           team: 'Iconspace Team',
+           attach: 13,
+           status: '4/5',
+           score: { days: 2, colors: { bg: '#FFEEEF', text: '#FC5A5A' } },
+           line: 85,
+           type: 'In Progress',
+           users: [
+             {
+               size: 32,
+               name: 'GU',
+               color: '#FF9AD5',
+               avatar: ''
+             }
+           ]
+         },
+         {
+           id: '10',
+           title: 'Reactjs.org Website',
+           team: 'Uranus Team',
+           attach: 13,
+           status: '10/10',
+           score: { days: 0, color: { bg: '', text: '' } },
+           line: 100,
+           type: 'Complete',
+           users: [
+             {
+               size: 32,
+               name: 'ZT',
+               color: '#82C43C',
+               avatar: ''
+             },
+             {
+               size: 32,
+               name: 'MI',
+               color: '#50B5FF',
+               avatar: ''
+             }
+           ]
+         }
+       ]
